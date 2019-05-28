2466fc49f4c76caf3ac08d21a7e375fcaaceb556
The Natural <philip.smalls86@gmail.com>
2019-05-28 03:23:11 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
   array = []
  
   while i < collection.length 
-  if yield (collection[i]) == true 
+  if yield (collection[i]) 
     array << collection[i]
   end
     i += 1 
